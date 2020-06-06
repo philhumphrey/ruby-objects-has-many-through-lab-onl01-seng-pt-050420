906ea0fc427992d2a3f29f0fb25ba9680c077676
Phil Humphrey <phumphrey588@yahoo.com>
2020-06-06 18:33:16 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,28 @@
+class Artist
+attr_accessor :name
+@@all = []
+  
+def initialize(name)
+  @name = name
+  @@all << self
+  end
+  
+def self.all
+    @@all
+  end
+  
+  def songs
+  song.all.select do |song| song == self
+  end
+
+def new_song(name, genre)
+  Song.new(name, self, genre)
+  end
+  
+def genres
+  songs.map {|song| song.genre}
+  end
+end
+end
+
+    
