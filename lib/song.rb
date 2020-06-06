a005d50bee8d8ecbc88649b9b50893242c4c727c
Phil Humphrey <phumphrey588@yahoo.com>
2020-06-06 19:04:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,10 +4,11 @@ class Song
 
 attr_accessor :name, :artist, :genre
 
-
-
 def initialize(name, artist, genre)
-
+@name = name
+@artist = artist
+@genre = genre
+@@all << self
 
 def self.all
   @@all
