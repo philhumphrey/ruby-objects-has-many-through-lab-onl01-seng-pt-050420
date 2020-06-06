2269609c6c7fdb91e166bd9b1a574a9c191e0c8f
Phil Humphrey <phumphrey588@yahoo.com>
2020-06-06 20:30:18 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -12,12 +12,11 @@ class Doctor
     @@all
   end
   
-  def new_appointment(date, patient)
-    Appointment.new(date, self, patient)
-  end
   
   def appointments 
     Appointment.all.select{|appointment| appointment.doctor == self}
   end
+  
+  
   
 end
